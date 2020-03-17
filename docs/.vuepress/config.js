8a2b22928803128562bceb16443af72bfbd8cc20
Prashanth Krishnamurthy <prashanth1krishnamurthy@gmail.com>
2020-03-17 20:46:29 +0530

    icons ahoy!

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,14 +6,14 @@ module.exports = {
   // prettier-ignore
   head: [
     ["link", { rel: "stylesheet", href:"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css"}],
-    ["link", { rel: "icon", href: "/logo.png" }],
+    ["link", { rel: "icon", href: "/images/sf-monkey-200x200.png" }],
     ["link", { rel: "manifest", href: "/manifest.json" }],
-    ["meta", { name: "theme-color", content: "#3eaf7c" }],
+    ["meta", { name: "theme-color", content: "#2196f3" }],
     ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
     ["meta", { name: "apple-mobile-web-app-status-bar-style", content: "black" }],
-    ["link", { rel: "apple-touch-icon", href: "/icons/apple-touch-icon-152x152.png" }],
-    ["link", { rel: "mask-icon", href: "/icons/safari-pinned-tab.svg", color: "#3eaf7c" }],
-    ["meta", { name: "msapplication-TileImage", content: "/icons/msapplication-icon-144x144.png" }],
+    ["link", { rel: "apple-touch-icon", href: "/images/icons/icon-152x152.png" }],
+    ["link", { rel: "mask-icon", href: "/images/icons/safari-pinned-tab.svg", color: "#3eaf7c" }],
+    ["meta", { name: "msapplication-TileImage", content: "/images/icons/icon-144x144.png" }],
     ["meta", { name: "msapplication-TileColor", content: "#000000" }]
   ],
   locales: {
